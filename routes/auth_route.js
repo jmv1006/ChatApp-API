2c7820b32934a568c828e38f26e2cc9cfb6f66b1
Jonathan Andrade Valenzuela <jmv1006@gmail.com>
2022-06-07 20:15:27 -0700

    added auth to access users

--- routes/auth_route.js
+++ routes/auth_route.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const auth_controller = require('../controllers/auth_controller')
+const passport = require("passport");
 
-router.get('/users', auth_controller.get_users)
+router.get('/users',  passport.authenticate("jwt", { session: false }), auth_controller.get_users)
 
-router.get('/users/:userId', auth_controller.get_specific_user)
+router.get('/users/:userId',  passport.authenticate("jwt", { session: false }), auth_controller.get_specific_user)
 
 router.post('/sign-up', auth_controller.create_user)
 
